04aff80da9353654a5ed182b2657dad4a386e429
guilherme.vieira <vaninguilherme@gmail.com>
2023-02-22 10:29:45 -0300

    feat: pages, contexts

---
+++ src/ui/pages/Home.tsx
@@ -0,0 +1,10 @@
+import { Button, Title } from "@components/common"
+
+export default function Home(){
+    return (
+        <section className="Home">
+            <Title.ExtraLarge>My title</Title.ExtraLarge>
+            <Button></Button>
+        </section>
+    )
+}
