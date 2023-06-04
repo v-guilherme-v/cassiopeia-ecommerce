b8355c1663647d7481f3792d29b10b68857050c8
guilherme.vieira <vaninguilherme@gmail.com>
2023-06-04 01:22:17 -0300

    fix: validator tests, wrong icon name

--- src/ui/components/icons/index.ts
+++ src/ui/components/icons/index.ts
@@ -1,4 +1,4 @@
 export { default as ThinArrow } from "./thin-arrow/icon"
-export { default as Chevron } from "./chevron/icon"
+export { default as Check } from "./check/icon"
 export { default as Close } from "./close/icon"
 export * from "./types"
