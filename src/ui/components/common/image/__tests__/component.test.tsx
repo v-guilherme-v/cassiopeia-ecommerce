ffb468527a6bb2ff2429031e210426d03dce7291
guilherme.vieira <vaninguilherme@gmail.com>
2023-10-04 22:51:04 -0300

    [fix] image has a default "no-image" image now

--- src/ui/components/common/image/__tests__/component.test.tsx
+++ src/ui/components/common/image/__tests__/component.test.tsx
@@ -1,13 +1,32 @@
-import { screen, render } from "@testing-library/react"
+import { screen, render, fireEvent } from "@testing-library/react"
+
+import { LightThemeProvider } from "@providers"
 import Image from "../component"
 
 describe("The image component", () => {
   it("renders a random image", () => {
     render(
-      <Image src="custom-image-path.png" alt="my flowers"/>
+      <LightThemeProvider>
+        <Image src="custom-image-path.png" alt="my flowers"/>
+      </LightThemeProvider>
     )
 
     const image = screen.getByAltText("my flowers")
     expect(image).toBeInTheDocument()
+  })
+
+  it("has a fallback image that is rendered on error", () => {
+    render(
+      <LightThemeProvider>
+        <Image src="" alt="error"/>
+      </LightThemeProvider>
+    )
+
+    const image = screen.getByAltText("error")
+    expect(image).toBeInTheDocument()
+
+    // Dispatching <img> tag onError function
+    fireEvent.error(image)
+    expect(image).toHaveAttribute("error", "true")
   })
 })
--- src/ui/components/common/image/component.tsx
+++ src/ui/components/common/image/component.tsx
@@ -1,12 +1,49 @@
-import { type PropsWithChildren } from "react"
+import { type PropsWithChildren, useState } from "react"
+import styled from "styled-components"
+import fallbackSrc from "src/assets/no-image.jpg"
+import { getFallbackImageStyles } from "@theme/selectors"
 
 interface ImageProps extends PropsWithChildren {
-  src?: string
-  alt?: string
+  src: string
+  alt: string
+  width?: string
+  height?: string
 }
 
-function Image ({ src, alt }: ImageProps): JSX.Element {
-  return <img src={src} alt={alt} />
+function Image ({ src, alt, width, height }: ImageProps): JSX.Element {
+  const [ failedToLoad, setFailedToLoad ] = useState<boolean>(false)
+
+  /**
+   * An image may fail to load
+   */
+  const onImageError = (): void => {
+    setFailedToLoad(true)
+  }
+
+  const imagePath = failedToLoad ? fallbackSrc : src
+  const errorProperty = failedToLoad ? { error: "true" } : {}
+
+  return (
+    <StyledImage>
+      <img
+        src={imagePath} alt={alt}
+        width={width} height={height}
+        onError={onImageError}
+        {...errorProperty }
+      />
+    </StyledImage>
+  )
 }
+
+const StyledImage = styled.div`
+  img[error="true"] {
+    width: 100%;
+    height: 100%;
+    object-position: center;
+    background-color: ${props => getFallbackImageStyles(props).backgroundColor};
+  }
+`
+
+Image.Styled = StyledImage
 
 export default Image
