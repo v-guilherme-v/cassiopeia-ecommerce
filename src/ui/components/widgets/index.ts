e6e747a33006d43700e0c1ed377735513fbdfabe
guilherme.vieira <vaninguilherme@gmail.com>
2023-11-11 06:04:11 -0300

    [fix] missing export

--- src/ui/components/widgets/index.ts
+++ src/ui/components/widgets/index.ts
@@ -1,5 +1,6 @@
 export { default as TopBar } from "./top-bar/widget"
 export { default as Navigation } from "./navigation/widget"
 export { default as Header } from "./header/widget"
+export { default as BannerContainer } from "./banner-container/widget"
 export { default as HeroBanner } from "./hero-banner/widget"
 export { default as Footer } from "./footer/widget"
