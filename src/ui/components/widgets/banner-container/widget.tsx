dd1de422c890428d4a815671864d78e7e018e5af
guilherme.vieira <vaninguilherme@gmail.com>
2023-11-19 00:21:50 -0300

    [feat] dynamic banner config hook. styles

--- src/ui/components/widgets/banner-container/widget.tsx
+++ src/ui/components/widgets/banner-container/widget.tsx
@@ -1,16 +1,30 @@
 import type { PropsWithChildren } from "react"
 import styled from "styled-components"
+import { Container } from "@components/common"
+import { getViewPortsStyles } from "@theme/selectors"
 
-function BannerContainer ({ children }: PropsWithChildren): JSX.Element {
+function BannerContainer (props: PropsWithChildren): JSX.Element {
   return (
     <StyledBannerContainer>
-      Banner container
-      { children }
+      { props.children }
     </StyledBannerContainer>
   )
 }
 
-const StyledBannerContainer = styled.div``
+const StyledBannerContainer = styled(Container.Styled)`
+  display: grid;
+  --column-size: calc(50% - 20px);
+  grid-template-columns: repeat(2, var(--column-size));
+  justify-content: space-between;
+
+  ${Container.Styled} {
+    padding: 0;
+  }
+
+  @media(max-width: ${props => getViewPortsStyles(props).medium}) {
+    display: none;
+  }
+`
 
 BannerContainer.Styled = StyledBannerContainer
 
---
+++ src/ui/components/widgets/hero-banner/hooks.ts
@@ -0,0 +1,35 @@
+import { useTheme } from "styled-components"
+import { getColorStyles } from "@theme/selectors"
+import { ButtonModels } from "@components/common/button"
+import type { HeroBannerProps } from "./types"
+
+export function useHeroBannerConfig (props: HeroBannerProps): any {
+  const {
+    button,
+    image,
+    title,
+    description
+  } = props
+
+  const theme = useTheme()
+  const colors = getColorStyles({ theme })
+
+  return {
+    image,
+    title: {
+      // It means you can pass options/configs in or just a raw string
+      text: typeof title === "object" ? title.text : title,
+      color: typeof title === "object" ? title.color : colors.black
+    },
+    description: {
+      text: typeof description === "object" ? description.text : description,
+      color: typeof description === "object" ? description.color : colors.black
+    },
+    button: {
+      text: typeof button === "object" ? button.text : button,
+      textColor: typeof button === "object" ? button.textColor : colors.black,
+      model: typeof button === "object" ? button.model : ButtonModels.PRIMARY,
+      backgroundColor: typeof button === "object" ? button.backgroundColor : colors.primary
+    }
+  }
+}
