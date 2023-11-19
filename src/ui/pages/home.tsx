f0fafff118a568eca5ef0724e68d2f3c80655c2a
guilherme.vieira <vaninguilherme@gmail.com>
2023-11-19 00:23:09 -0300

    [feat] new banner config, new selectors

--- src/ui/pages/home.tsx
+++ src/ui/pages/home.tsx
@@ -1,12 +1,25 @@
-import styled from "styled-components"
-import { Header, Navigation, TopBar, HeroBanner, Footer } from "@components/widgets"
+import styled, { useTheme } from "styled-components"
+
+import {
+  Header,
+  Navigation,
+  TopBar,
+  BannerContainer,
+  HeroBanner,
+  Footer
+} from "@components/widgets"
+
 import { ProductCarousel } from "@components/commerce"
 
 import flowersBanner from "src/assets/flowers-banner.png"
 import randomFlower from "src/assets/flower-1.png"
-import { getViewPortsStyles } from "@theme/selectors"
+
+import { getColorStyles, getViewPortsStyles } from "@theme/selectors"
+import type { ColorStyles } from "@theme/types"
 
 export default function Home (): JSX.Element {
+  const colors: ColorStyles = getColorStyles({ theme: useTheme() })
+
   return (
     <StyledHome className="Home">
       <Header>
@@ -15,11 +28,29 @@ export default function Home (): JSX.Element {
       </Header>
 
       <HeroBanner
-        title="Flowers"
-        description="Our flowers..."
-        buttonLabel="Take a look"
+        title={{ text: "Flowers", color: colors.snow }}
+        description={{ text: "Our flowers", color: colors.snow }}
+        button={{ text: "See more", backgroundColor: colors.black }}
         image={{ src: flowersBanner, alt: "Flowers banner" }}
       />
+
+      <BannerContainer>
+        <HeroBanner
+          title={{ text: "Flowers", color: colors.snow }}
+          description={{ text: "Our flowers", color: colors.snow }}
+          button={{ text: "See more", backgroundColor: colors.black }}
+          image={{ src: flowersBanner, alt: "Flowers banner" }}
+        />
+        <HeroBanner
+          title={{ text: "Flowers", color: colors.snow }}
+          description={{ text: "Our flowers", color: colors.snow }}
+          button={{ text: "See more", backgroundColor: colors.black }}
+          image={{
+            src: flowersBanner,
+            alt: "Flowers banner"
+          }}
+        />
+      </BannerContainer>
 
       <ProductCarousel carouselTitle="Recommended for you"
         products={
