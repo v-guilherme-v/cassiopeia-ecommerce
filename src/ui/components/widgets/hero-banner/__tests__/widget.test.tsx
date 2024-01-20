6381ad71c420d0e4f0b6b92123a3f545d1b83cdb
guilherme.vieira <vaninguilherme@gmail.com>
2024-01-20 19:35:58 -0300

    [fix] missing hero banner test

--- src/ui/components/widgets/hero-banner/__tests__/widget.test.tsx
+++ src/ui/components/widgets/hero-banner/__tests__/widget.test.tsx
@@ -17,4 +17,27 @@ describe("The image component", () => {
     const heroActionButton = screen.getByText("my hero banner")
     expect(heroActionButton).toBeInTheDocument()
   })
+
+  it("may have more detailed properties", () => {
+    render(
+      <LightThemeProvider>
+        <HeroBanner
+          title={{ text: "My hero banner", color: "#f00" }}
+          description={{ text: "some description", color: "#8a1a5f" }}
+          image={{ src: "some-image-path.png", alt: "image alt" }}
+          button={{
+            text: "click me!",
+            textColor: "#fff",
+            backgroundColor: "#ff0"
+          }}
+        />
+      </LightThemeProvider>
+    )
+
+    const heroBannerTitle = screen.getByText("My hero banner")
+    expect(heroBannerTitle).toBeInTheDocument()
+
+    const heroBannerAction = screen.getByText("click me!")
+    expect(heroBannerAction).toBeInTheDocument()
+  })
 })
