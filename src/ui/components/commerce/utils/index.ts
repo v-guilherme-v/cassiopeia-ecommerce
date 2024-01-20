01b6b61d0e0f4d307d89904fd983e7113d8aaf15
guilherme.vieira <vaninguilherme@gmail.com>
2024-01-20 19:31:10 -0300

    [fix] file moved

--- src/ui/components/commerce/utils/index.ts
+++
@@ -1,8 +0,0 @@
-export const toCurrency = (value?: number): string | undefined => {
-  if (value !== undefined) {
-    return value.toLocaleString("pt-BR", {
-      style: "currency",
-      currency: "BRL"
-    })
-  }
-}
