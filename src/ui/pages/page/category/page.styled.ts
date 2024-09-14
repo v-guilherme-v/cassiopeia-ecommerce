84160dbed9dbdf3400ee6d682197b103c6a7dbf1
guilherme.vieira <vaninguilherme@gmail.com>
2024-09-14 01:51:48 -0300

    feat: min 778 viewport, category filters and results

--- src/ui/pages/page/category/page.styled.ts
+++ src/ui/pages/page/category/page.styled.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components"
-import { Block, Container, Text } from "@components/common"
+import { Block, Button, Container, Text, Title } from "@components/common"
 import { Footer } from "@components/widgets"
 import { getColorStyles, getViewPortsStyles } from "@theme/selectors"
 
@@ -11,6 +11,14 @@ export default styled.section`
   ${Container.Styled}[data-name="CategoryPage__Container"] {
     flex: 1;
   }
+
+  ${Block.Styled}[data-name="CategoryPage__Heading"] {
+    ${Title.Styled.Normal}[data-name="CategoryPage__DisplayName"] {
+      font-size: 22px;
+      line-height: 28px;
+    }
+  }
+
 
   ${Block.Styled}[data-name="CategoryPage__Filters"] {
     display: flex;
@@ -31,6 +39,63 @@ export default styled.section`
     ${Text.Styled.Normal}[data-name="CategoryPage__FilterTotalItems"] {
       user-select: none;
     }
+
+    svg {
+      color: #505050
+    }
+
+    @media (max-width: ${props => getViewPortsStyles(props).medium}) {
+      flex-direction: row-reverse;
+      border-bottom: none;
+      margin-top: 13px;
+      padding: 0;
+    }
+  }
+
+  ${Block.Styled}[data-name="CategoryPage__Results"] {
+    margin-top: 40px;
+
+    ${Block.Styled}[data-name="CategoryPage__ResultList"] {
+      display: grid;
+      grid-template: repeat(2, 1fr) / repeat(4, 260px);
+      justify-content: space-between;
+      
+      @media (max-width: ${props => getViewPortsStyles(props).extraLarge}) {
+        grid-template-columns: repeat(3, 260px);
+        justify-content: space-evenly;
+      }
+
+      @media (max-width: 900px) {
+        grid-template-columns: repeat(2, 260px);
+      }
+
+      @media (max-width: ${props => getViewPortsStyles(props).medium}) {
+        grid-template-columns: repeat(3, 163px);
+      }
+
+      @media (max-width: 540px) {
+        grid-template-columns: repeat(2, 163px);
+      }
+
+      @media (max-width: 390px) {
+        justify-content: space-between;
+      }
+
+      @media (max-width: 370px) {
+        grid-template-columns: 163px;
+        justify-content: center;
+      }
+    }
+
+    ${Button.Styled.Normal}[data-name="CategoryPage__ResultsLoadMore"] {
+      max-width: 223px;
+      width: 100%;
+      margin: 40px auto 0;
+    }
+
+    @media (max-width: ${props => getViewPortsStyles(props).medium}) {
+      margin-top: 38px;
+    }
   }
   
   ${Footer.Styled} {
@@ -39,7 +104,7 @@ export default styled.section`
 
   @media(max-width: ${props => getViewPortsStyles(props).medium}) {
     ${Footer.Styled} {
-      margin-top: 0;
+      margin-top: 2rem;
     }
   }
 `
--- src/ui/pages/page/category/page.tsx
+++ src/ui/pages/page/category/page.tsx
@@ -1,10 +1,13 @@
+/* eslint-disable multiline-ternary */
 import { useState } from "react"
 
 import {
   Text,
   Block,
   Container,
-  Select
+  Select,
+  Button,
+  Title
 } from "@components/common"
 
 import {
@@ -16,8 +19,15 @@ import {
   Footer
 } from "@components/widgets"
 
+import { SettingsIcon } from "@components/icons"
+
+import { ProductCard } from "@components/commerce"
+
 import StyledCategoryPage from "./page.styled"
 import { MiniCartContext } from "@contexts"
+
+import randomFlower from "src/assets/flower-1.png"
+import { useViewPorts } from "@ui/hooks/use-viewports"
 
 const breadcrumbs = [
   { name: "Level 1" },
@@ -26,8 +36,20 @@ const breadcrumbs = [
   { name: "Level 4" }
 ]
 
+const categoryResults = Array(8).fill(1).map((_, index) => ({
+  id: index.toString(),
+  displayName: `Flower ${index + 1}`,
+  imageSource: randomFlower,
+  pricing: {
+    listPrice: 90.99,
+    salePrice: 80.99
+  }
+}))
+
 export default function CategoryPage (): JSX.Element {
   const [ isMiniCartOpen, setIsMiniCartOpen ] = useState<boolean>(false)
+
+  const viewPorts = useViewPorts()
 
   return (
     <StyledCategoryPage>
@@ -44,26 +66,52 @@ export default function CategoryPage (): JSX.Element {
 
         <Container data-name="CategoryPage__Container">
           <Block data-name="CategoryPage__Heading">
-            <Breadcrumbs breadcrumbs={breadcrumbs} displayMode="title" />
+            {viewPorts.minWidthMedium ? (
+              <Breadcrumbs breadcrumbs={breadcrumbs} displayMode="title" />
+            ) : (
+              <Title data-name="CategoryPage__DisplayName">{breadcrumbs[breadcrumbs.length - 1].name}</Title>
+            )}
           </Block>
           <Block data-name="CategoryPage__Filters">
-            <Block data-name="CategoryPage__FilterOptions">
-              <Select placeholder="Sort by" mode="multi" options={[
-                { value: "popular", label: "Popular" },
-                { value: "best-sellers", label: "Best sellers" },
-                { value: "news", label: "Just arrived" }
-              ]} />
-              <Select placeholder="Occasion" mode="multi" options={[
-                { value: "first-date", label: "First date" },
-                { value: "valentines", label: "Valentines" },
-                { value: "mothers-day", label: "Mother's Day" }
-              ]} />
-              <Select placeholder="Price" options={[
-                { value: "asc", label: "Most expensive" },
-                { value: "desc", label: "Cheapest" }
-              ]} />
-            </Block>
+            {viewPorts.minWidthMedium ? (
+              <Block data-name="CategoryPage__FilterOptions">
+                <Select placeholder="Sort by" mode="multi" options={[
+                  { value: "popular", label: "Popular" },
+                  { value: "best-sellers", label: "Best sellers" },
+                  { value: "news", label: "Just arrived" }
+                ]} />
+                <Select placeholder="Occasion" mode="multi" options={[
+                  { value: "first-date", label: "First date" },
+                  { value: "valentines", label: "Valentines" },
+                  { value: "mothers-day", label: "Mother's Day" }
+                ]} />
+                <Select placeholder="Price" options={[
+                  { value: "asc", label: "Most expensive" },
+                  { value: "desc", label: "Cheapest" }
+                ]} />
+              </Block>
+            ) : (
+              <SettingsIcon />
+            )}
             <Text data-name="CategoryPage__FilterTotalItems">24 items</Text>
+          </Block>
+          <Block data-name="CategoryPage__Results">
+            <Block data-name="CategoryPage__ResultList">
+              {categoryResults.map(result => {
+                return (
+                  <ProductCard
+                    key={result.id}
+                    product={{
+                      pageRoute: "/product",
+                      displayName: result.displayName,
+                      imageSource: randomFlower,
+                      pricing: { listPrice: 49.99 }
+                    }}
+                  />
+                )
+              })}
+            </Block>
+            <Button data-name="CategoryPage__ResultsLoadMore">See more</Button>
           </Block>
         </Container>
 
