33f626225c8c252ee0101969778b6db50b3b352c
guilherme.vieira <vaninguilherme@gmail.com>
2024-11-27 00:03:19 -0300

    feat: regexes

---
+++ src/utils/expressions.ts
@@ -0,0 +1,3 @@
+export const userFullNameRegEx = "^[a-zA-Z]+([ '-][a-zA-Z]+)*$"
+export const userEmailRegEx = "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"
+export const userCellphoneRegEx = "^\\+?[1-9]\\d{1,14}$"
