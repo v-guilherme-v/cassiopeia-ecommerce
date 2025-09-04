632534375abf9909b95ef2d4b19698591dc00493
guilherme.vieira <vaninguilherme@gmail.com>
2025-09-04 01:04:21 -0300

    feat: i18next init / refac: summary

---
+++ src/ui/components/commerce/summary-price-info/component.tsx
@@ -0,0 +1,40 @@
+import styled from "styled-components"
+import { Block, Text } from "@components/common"
+import { getColorStyles } from "@app/ui/theme/selectors"
+import { useTranslation } from "react-i18next";
+
+function SummaryPriceInfo (): JSX.Element {
+  const { t } = useTranslation()
+
+  return (
+    <StyledSummaryPriceInfo>
+      <Block data-name="SummaryPriceInfo">
+        <Block data-name="SummaryPriceInfo__Item">
+          <Text>{t("summary.discounts")}</Text>
+          <Text weight="medium">R$ 15,00</Text>
+        </Block>
+        <Block data-name="SummaryPriceInfo__Item">
+          <Text>{t("summary.total")}</Text>
+          <Text weight="medium">R$ 129,90</Text>
+        </Block>
+      </Block>
+    </StyledSummaryPriceInfo>
+  )
+}
+
+const StyledSummaryPriceInfo = styled.div`
+  ${Block.Styled}[data-name="SummaryPriceInfo__Item"] {
+    display: flex;
+    justify-content: space-between;
+    border-top: 1px solid ${props => getColorStyles(props).paleGrey};
+    padding: 18px 0 14px;
+  }
+
+  ${Block.Styled}[data-name="SummaryPriceInfo__Item"]:last-child {
+    border-bottom: 1px solid ${props => getColorStyles(props).paleGrey};
+  }
+`
+
+SummaryPriceInfo.Styled = StyledSummaryPriceInfo
+
+export default SummaryPriceInfo
